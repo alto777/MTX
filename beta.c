4085ae8106ea8617f4d0baae09daabaed15c563c
Robert A Moeser <ram32768@gmail.com>
2022-02-23 17:07:53 -0500

    OK now just a tiny beta.c change so...

--- beta.c
+++ beta.c
@@ -1,6 +1,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+/* on 23 FEB 2022, just trying to change one file, add, commit and push */
+/* without the freebase stuff */
+
 void betaFunction(char *x)
 {
   printf(" and beta says <%s>\n.", x);
