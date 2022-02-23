a90b47133f36cfd5466541d40779548dd95c35fe
Robert A Moeser <ram32768@gmail.com>
2022-02-23 17:02:51 -0500

    add, then... commit?

--- alpha.c
+++ alpha.c
@@ -6,6 +6,7 @@
 /* i made this change in a cloned local repository */
 
 /* and trying this  in v2 after git fetch turned it up */
+/* this is going to make me go insane on 23 FEB 2022 */
 
 int main(void)
 {
